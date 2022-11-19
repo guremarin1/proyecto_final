3e3426e67f71a0ef6c587857f73d5c44baa5308d
guremarin1 <greciaguremarin@gmail.com>
2022-11-19 01:48:05 -0300

    porque

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function toogleNavMobile( mobileNavID ) {
     let nav = document.querySelector(mobileNavID);
     nav.classList.toggle('hide-mobile-nav');
+    document.querySelector('body').classList.toggle('overflow-y-hidden');
 }
